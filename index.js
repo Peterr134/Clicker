7702489774f5af80612d4768d16b7b422f1d6066
unknown <BT_3W2_21@BKTECH.LOCAL>
2024-10-15 11:02:35 -0400

    Attempt 2

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 // Build a Counter App
+count = 0;
+numSchoolBuses = 0;
 let title = document.createElement("title");
-title.innerHTML = "Chocolate Counter";
+title.innerHTML = "School Simulator";
 document.head.appendChild(title);
+while(numSchoolBuses > 0){
+    setTimeout(useSchoolBuses, 1000);
+}
+
 function increment(){
-    let count = document.getElementById("count-el")
-    count.innerHTML = parseInt(count.innerHTML) + 1
+    let countB = document.getElementById("count-el")
+    count++;
+    console.log(count);
+    countB.innerHTML = count;
+}
+
+function save(){
+    let saved = document.getElementById("saved");
+    saved.innerHTML += count + " - ";
+}
+function buySchoolBus(){
+    numSchoolBuses++;
+    let counter = document.getElementById("busCounter");
+    counter.innerHTML = "School Buses: " + numSchoolBuses;
+}
+function useSchoolBuses(){
+    count += numSchoolBuses;
 }
