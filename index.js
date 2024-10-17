8730ddb20154cf20ded539e6035f68849b64f43e
Peterr134 <peterr134@nycstudents.net>
2024-10-17 10:26:22 -0400

    Fixed it

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // Build a Counter App
 count = 0;
 numSchoolBuses = 0;
+numStrictParents = 0;
 let title = document.createElement("title");
 title.innerHTML = "School Simulator";
 document.head.appendChild(title);
-while(numSchoolBuses > 0){
-    setTimeout(useSchoolBuses, 1000);
-}
+    setInterval(() => {
+        let newAmt = numSchoolBuses;
+        newAmt *= 1 + (0.05 * numStrictParents);
+        newAmt = Math.floor(newAmt);
+        let ps = document.getElementById("perSecond");
+        ps.innerHTML = "Current Students per Second: " + newAmt;
+        count += newAmt;
+        
+        let countB = document.getElementById("count-el")
+        countB.innerHTML = count;
+    }, 1000);
 
 function increment(){
     let countB = document.getElementById("count-el")
@@ -15,15 +24,41 @@ function increment(){
     countB.innerHTML = count;
 }
 
+function decrement(){
+    if(count >= 1){
+    let countB = document.getElementById("count-el")
+    count--;
+    console.log(count);
+    countB.innerHTML = count;
+    }
+}
+
 function save(){
     let saved = document.getElementById("saved");
     saved.innerHTML += count + " - ";
 }
 function buySchoolBus(){
+    if(count >= 10){
+    let countB = document.getElementById("count-el")
     numSchoolBuses++;
+    count -= 10;
+    countB.innerHTML = count;
     let counter = document.getElementById("busCounter");
     counter.innerHTML = "School Buses: " + numSchoolBuses;
+    }
 }
-function useSchoolBuses(){
-    count += numSchoolBuses;
+function buyStrictParent(){
+    if(count >= 50){
+    let countB = document.getElementById("count-el")
+    numStrictParents++;
+    count -= 50;
+    countB.innerHTML = count;
+    let counter = document.getElementById("strPCounter");
+    counter.innerHTML = "Strict Parents: " + numStrictParents;
+    }
+}
+function rename(){
+    let header = document.getElementById("title")
+    let newName = prompt("What should they be called?")
+    header.innerHTML = newName + " attending"
 }
